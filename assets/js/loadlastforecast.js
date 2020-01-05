222374bf62440506f22f687093ee417f31013a2d
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-01-04 18:41:19 -0700

    removed console logs

--- assets/js/loadlastforecast.js
+++ assets/js/loadlastforecast.js
@@ -101,7 +101,7 @@ function loadLastWeather() {
         // -- computes average temp and humidity for the day --
         var avgTemp = Math.floor(sumTemp/8);
         var avgHumid = Math.floor(sumHumid/8);
-        console.log(avgTemp);
+        
 
          // -- building dayOne weather card start --
          var weatherList = $("<ul class='tile is-child box'>").css({
@@ -142,7 +142,7 @@ function loadLastWeather() {
         // -- computes average temp and humidity for the day --
         var avgTemp = Math.floor(sumTemp/8);
         var avgHumid = Math.floor(sumHumid/8);
-        console.log(avgTemp);
+        
 
          // -- building dayTwo weather card start --
          var weatherList = $("<ul class='tile is-child box'>").css({
@@ -183,7 +183,7 @@ function loadLastWeather() {
         // -- computes average temp and humidity for the day --
         var avgTemp = Math.floor(sumTemp/8);
         var avgHumid = Math.floor(sumHumid/8);
-        console.log(avgTemp);
+        
 
          // -- building dayThree weather card start --
          var weatherList = $("<ul class='tile is-child box'>").css({
@@ -224,7 +224,7 @@ function loadLastWeather() {
         // -- computes average temp and humidity for the day --
         var avgTemp = Math.floor(sumTemp/8);
         var avgHumid = Math.floor(sumHumid/8);
-        console.log(avgTemp);
+        
 
          // -- building dayFour weather card start --
          var weatherList = $("<ul class='tile is-child box'>").css({
@@ -265,8 +265,7 @@ function loadLastWeather() {
         // -- computes average temp and humidity for the day --
         var avgTemp = Math.floor(sumTemp/8);
         var avgHumid = Math.floor(sumHumid/8);
-        console.log(avgTemp);
-        console.log(response);
+        
 
          // -- building dayOne weather card start --
          var weatherList = $("<ul class='tile is-child box'>").css({
